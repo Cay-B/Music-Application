b29a838053bbcf803e62d3ec2d839a0fbd5bf0d9
Kaleb Alemayehu <xyyy00128@gmail.com>
2024-04-02 09:03:09 +0300

    New Releases Change

--- src/components/NewReleases.js
+++ src/components/NewReleases.js
@@ -1,20 +1,20 @@
 import { View, Text, Image, ScrollView } from 'react-native'
 
 export default function NewReleases() {
-  return(
+  return (
     <View className='flex flex-col self-center py-4 px-4 bg-white '>
-     
+
       <View className='bg-white space-y-3 pt-3 justify-start items-start w-full'>
-        
+
         <View className='w-1/2 bg-blue-100 p-2 my-3 ml-2 rounded-lg'>
           <Text className='text-xl'>New Releases</Text>
         </View>
-        
-        <ScrollView horizontal showsHorizontalScrollIndicator={false} className=''> 
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} className=''>
 
           <View className='bg-white flex flex-col justify-center space-y-2'>
             <View className='bg-white'>
-              <Image className='w-40 h-36 m-2' resizeMode='cover' source={require ('../assets/images/release 1.jpg')} />
+              <Image className='w-40 h-36 m-2' resizeMode='cover' source={require('../assets/images/release 1.jpg')} />
             </View>
             <View className='bg-white w-11/12 px-2'>
               <View className=''>
@@ -25,7 +25,7 @@ export default function NewReleases() {
 
           <View className='bg-white flex flex-col justify-center space-y-2'>
             <View className='bg-white'>
-              <Image className='w-40 h-36 m-2' resizeMode='cover' source={require ('../assets/images/release 2.jpg')} />
+              <Image className='w-40 h-36 m-2' resizeMode='cover' source={require('../assets/images/release 2.jpg')} />
             </View>
             <View className='bg-white w-11/12 px-2'>
               <View className=''>
@@ -36,7 +36,7 @@ export default function NewReleases() {
 
           <View className='bg-white flex flex-col justify-center space-y-2'>
             <View className='bg-white'>
-              <Image className='w-40 h-36 m-2' resizeMode='cover' source={require ('../assets/images/release 3.jpg')} />
+              <Image className='w-40 h-36 m-2' resizeMode='cover' source={require('../assets/images/release 3.jpg')} />
             </View>
             <View className='bg-white w-11/12 px-2'>
               <View className=''>
@@ -44,9 +44,9 @@ export default function NewReleases() {
               </View>
             </View>
           </View>
-      </ScrollView>
+        </ScrollView>
       </View>
-      </View>
+    </View>
 
   )
 }
